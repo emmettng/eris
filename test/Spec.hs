f5b28b7da27d61ef0d24c607a9aa18b4e60d286d
hao.wu <haowu@epoque.shoes>
2018-05-08 10:40:15 +0800

    fix the bug when running stack test on OSX, add /usr/lib in extra-lib-dirs in stack.yaml

--- test/Spec.hs
+++ test/Spec.hs
@@ -9,10 +9,10 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-    describe "Test cosine similarity of two vectors" $ do
-      it "The vector and itself" $ do
+    describe "Cosine Similarity: " $ do
+      it "The vector and itself is one." $ do
         (cosineSimilarity [1.0,1.0,1.0] [1.0,1.0,1.0]) `shouldBe` (1.0:: Double)
 
-      it "The vector and any perpendicular" $ do
+      it "The vector and any perpendicular vector is 0." $ do
         cosineSimilarity [1.0, 0.0] [0.0, 1.0] `shouldBe` 0
         cosineSimilarity [1.0,1.0] [1.0,-1.0] `shouldBe` 0
