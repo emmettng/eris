e280f689bcf7527ef7073e08a9c2d0139db31358
wuhao <emmettng@gmail.com>
2020-03-25 17:58:24 +0800

    keep doing

--- src/Eris/Distance/Minkowski.hs
+++ src/Eris/Distance/Minkowski.hs
@@ -1,9 +1,10 @@
 module Eris.Distance.Minkowski where
 
--- import Eris.Meta.DataTypes
-import Numeric.LinearAlgebra
-import Numeric.LinearAlgebra.Data
+import Eris.Meta.DataTypes
+import Numeric.LinearAlgebra hiding (Vector)
+import qualified Data.Vector.Storable as DV
 
+-- import Numeric.LinearAlgebra.Data
 {-
    This module contains different derivation of Minkowski distance. 
    These functions define distance between two high dimensional vectors X and Y.
@@ -11,8 +12,70 @@ import Numeric.LinearAlgebra.Data
 
    1. sad (sub absolute difference, taxicab metric, manhattan distance, l1-norm of (X-Y) ) 
 -}
--- | sad 
--- sad :: Vector -> Vector -> Double
--- sad v1 v2 = norm_1 (v1 - v2)
-sad' :: Vector Double -> Vector Double -> Double
-sad' v1 v2 = norm_1 $ v1 - v2
+-- | L1 norm family 
+-- Sum of Absolute Difference: norm(l1) of (v1 - v2)
+-- sad , taxicab, manhattan distance are different names of the same implementation.
+sad :: Vector -> Vector -> Double
+sad v1 v2 = norm_1 (v1 - v2)
+
+taxicab :: Vector -> Vector -> Double
+taxicab = sad
+
+manhattanDistance :: Vector -> Vector -> Double
+manhattanDistance = sad
+
+-- | Mean Absolute Difference 
+mad :: Vector -> Vector -> Double
+mad v1 v2 = norm1 / n
+  where
+    norm1 = sad v1 v2
+    n = fromIntegral . DV.length $ v1
+
+-- | L2 norm based 
+--
+euclideanDistance :: Vector -> Vector -> Double
+euclideanDistance v1 v2 = norm_2 (v1 - v2)
+
+-- | (L2 norm based) Sum Suqred Difference
+-- Squred euclidean distance
+ssd :: Vector -> Vector -> Double
+ssd v1 v2 = v <.> v
+  where
+    v = v1 - v2
+
+-- | (L2 norm based) Mean Sqaured Error (MSE)
+--  ssd averaged over the vector
+mse :: Vector -> Vector -> Double
+mse v1 v2 = squaredL2 / n
+  where
+    squaredL2 = ssd v1 v2
+    n = fromIntegral . DV.length $ v1
+
+-- | (L2 norm based) Root mean squared Error (rmse)
+-- euclidean distance averged over the vector
+rmse :: Vector -> Vector -> Double
+rmse v1 v2 = euclideanDistance v1 v2 / sqrtN
+  where
+    sqrtN = sqrt . fromIntegral $ DV.length v1
+
+-- | Minkowski Distance
+-- This is a degenerate case of Minkowski distance 
+-- in this case p is an Integral number for sure.
+minkowskiDistance :: Int -> Vector -> Vector -> Double
+minkowskiDistance p v1 v2 =
+  let vDiff = v1 - v2
+      absV = DV.map abs vDiff
+  in lpnorm p absV
+  where
+    lpnorm :: Int -> Vector -> Double
+    lpnorm 0 vec =
+      let vl = -DV.length vec
+      in norm_1 $ DV.map (f0 vl) vec
+    lpnorm p vec = nroot p $ norm_1 $ DV.map (\n -> n ^ p) vec
+    nroot
+      :: (Integral a, Floating b)
+      => a -> b -> b
+    nroot 0 _ = 1
+    nroot n f = f ** (1 / fromIntegral n)
+    f0 :: Int -> Double -> Double
+    f0 l v = (2 ^ l) * (v / (1 + v))
--- src/Eris/Meta/DataTypes.hs
+++ src/Eris/Meta/DataTypes.hs
@@ -1,5 +1,12 @@
 module Eris.Meta.DataTypes where
 
-import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Storable as V
 
-type Vector = U.Vector Double
+-- | Functions in hMatrix involves manipulating Vector and Matrix. 
+-- Corresponding definitions of Vector and Matrix  relies on Data.Vector.Storable.
+-- https://hackage.haskell.org/package/hmatrix-0.20.0.0/docs/Numeric-LinearAlgebra-Data.html#t:Vector
+--
+-- In this package, Vector is not as complicate as in hMatrix. 
+-- Vector is specified as containing elements of the type "Double".
+--
+type Vector = V.Vector Double
